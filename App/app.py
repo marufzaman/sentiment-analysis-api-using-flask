918be3100a4383c8d90d11624317728025aa5453
A. M. Almarufuzzaman <marufzaman05@gmail.com>
2023-06-21 19:40:39 +0600

    main app and tests sepearated and contained inside App

---
+++ App/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, render_template, redirect
+from pydantic import BaseModel, validator
+from setfit import SetFitModel
+
+app = Flask(__name__)
+
+# Load the pre-trained sentiment model
+model = SetFitModel.from_pretrained("StatsGary/setfit-ft-sentinent-eval")
+
+
+class SentimentAnalysisRequest(BaseModel):
+    text: str
+
+    @validator('text')
+    def text_must_not_be_empty(cls, value):
+        if not value.strip():
+            raise ValueError('Invalid input: "text" must not be empty')
+        return value
+
+
+class SentimentAnalysisResponse(BaseModel):
+    text: str
+    sentiment: str
+
+
+@app.route('/', methods=['GET'])
+def redirect_to_analyze():
+    return redirect('/analyze')
+
+
+# Define a handler for the /analyze route, which accepts both GET and POST requests
+@app.route('/analyze', methods=['GET', 'POST'])
+def analyze_sentiment():
+    html_file = 'index.html'
+    if request.method == 'POST':
+        try:
+            payload = SentimentAnalysisRequest(**request.form)
+            sentiment = perform_sentiment_analysis(payload.text)
+            response = SentimentAnalysisResponse(text=payload.text, sentiment=sentiment)
+            return render_template(html_file, response=response)
+        except ValueError as e:
+            error_message = str(e)
+            return render_template(html_file, error=error_message)
+
+    return render_template(html_file)
+
+
+# Validate the request payload using Pydantic model
+def validate_request(data):
+    SentimentAnalysisRequest(**data)
+
+
+# Perform sentiment analysis
+def perform_sentiment_analysis(text):
+    preds = model([text])
+    sentiment_value = preds[0].item()
+    return map_sentiment_to_label(sentiment_value)
+
+
+# Map sentiment values to labels
+def map_sentiment_to_label(sentiment):
+    labels = ["negative", "positive", "neutral"]
+    return labels[sentiment]
+
+
+if __name__ == '__main__':
+    # Start the Flask server
+    app.run()
---
+++ App/test_app_unit.py
@@ -0,0 +1,40 @@
+import unittest
+
+from app import validate_request, perform_sentiment_analysis, map_sentiment_to_label
+
+
+class SentimentAnalysisTests(unittest.TestCase):
+    def test_validate_request_valid_data(self):
+        data = {"text": "This is a valid text."}
+        self.assertIsNone(validate_request(data))
+
+    def test_validate_request_missing_text(self):
+        data = {}
+        with self.assertRaises(ValueError):
+            validate_request(data)
+
+    def test_validate_request_invalid_text(self):
+        data = {"text": 123}
+        with self.assertRaises(ValueError):
+            validate_request(data)
+
+    def test_perform_sentiment_analysis(self):
+        text = "This is a positive sentence."
+        sentiment = perform_sentiment_analysis(text)
+        self.assertIn(sentiment, ["negative", "positive", "natural"])
+
+    def test_map_sentiment_to_label_positive(self):
+        sentiment_label = map_sentiment_to_label(1)
+        self.assertEqual(sentiment_label, "positive")
+
+    def test_map_sentiment_to_label_negative(self):
+        sentiment_label = map_sentiment_to_label(0)
+        self.assertEqual(sentiment_label, "negative")
+
+    def test_map_sentiment_to_label_neutral(self):
+        sentiment_label = map_sentiment_to_label(2)
+        self.assertEqual(sentiment_label, "natural")
+
+
+if __name__ == '__main__':
+    unittest.main()
